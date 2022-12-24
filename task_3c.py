eb5c97c285588a8b74a48eed5093bd7eefe23f54
vybhavraghav <vybhavraghav@gmail.com>
2022-12-24 12:57:52 +0530

    task 3C

---
+++ task_3c.py
@@ -0,0 +1,253 @@
+'''
+*****************************************************************************************
+*
+*        		===============================================
+*           		Pharma Bot (PB) Theme (eYRC 2022-23)
+*        		===============================================
+*
+*  This script is to implement Task 3C of Pharma Bot (PB) Theme (eYRC 2022-23).
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*
+*****************************************************************************************
+'''
+
+# Team ID:			[ Team-ID ]
+# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Filename:			task_3c.py
+# Functions:		[ perspective_transform, transform_values, set_values ]
+# 					
+
+
+####################### IMPORT MODULES #######################
+## You are not allowed to make any changes in this section. ##
+## You have to implement this task with the five available  ##
+## modules for this task                                    ##
+##############################################################
+import cv2 
+import numpy 
+from  numpy import interp
+from zmqRemoteApi import RemoteAPIClient
+import zmq
+##############################################################
+
+#################################  ADD UTILITY FUNCTIONS HERE  #######################
+
+def detect_ArUco_details(image):
+
+    """
+    Purpose:
+    ---
+    This function takes the image as an argument and returns a dictionary such
+    that the id of the ArUco marker is the key and a list of details of the marker
+    is the value for each item in the dictionary. The list of details include the following
+    parameters as the items in the given order
+        [center co-ordinates, angle from the vertical, list of corner co-ordinates] 
+    This order should be strictly maintained in the output
+
+    Input Arguments:
+    ---
+    `image` :	[ numpy array ]
+            numpy array of image returned by cv2 library
+    Returns:
+    ---
+    `ArUco_details_dict` : { dictionary }
+            dictionary containing the details regarding the ArUco marker
+    
+    Example call:
+    ---
+    ArUco_details_dict = detect_ArUco_details(image)
+    """    
+
+
+
+    ArUco_details_dict = {} #should be sorted in ascending order of ids
+    ArUco_corners = {}
+    
+    ##############	ADD YOUR CODE HERE	##############
+   
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY)
+    # cv2.imshow('gray', thresh)
+    key = getattr(aruco , f"DICT_5X5_50" )
+    arucoDict = aruco.Dictionary_get(key)
+    bbox, ids, r = aruco.detectMarkers(thresh, arucoDict)
+
+    
+    for i in range(len(ids)):
+        
+        x_sum = bbox[i][0][0][0]+ bbox[i][0][1][0]+ bbox[i][0][2][0]+ bbox[i][0][3][0]
+        y_sum = bbox[i][0][0][1]+ bbox[i][0][1][1]+ bbox[i][0][2][1]+ bbox[i][0][3][1]
+            
+        x_centerPixel = int(x_sum*0.25) 
+        y_centerPixel = int(y_sum*0.25)
+
+        top_center_x = (bbox[i][0][0][0]+bbox[i][0][1][0])/2
+        top_center_y = (bbox[i][0][0][1]+bbox[i][0][1][1])/2
+
+
+
+
+        x1 = x_centerPixel - top_center_x
+        y1 = y_centerPixel - top_center_y
+
+        angle = int(math.degrees((math.atan2(x1,y1))))
+
+
+        
+
+        ArUco_details_dict[int(ids[i][0])] = [[x_centerPixel,y_centerPixel],angle]
+        
+        ArUco_corners[int(ids[i][0])] = bbox[i][0].astype('int32')
+
+    ##################################################
+    
+    return ArUco_details_dict, ArUco_corners 
+
+def mark_ArUco_image(image,ArUco_details_dict, ArUco_corners):
+
+    for ids, details in ArUco_details_dict.items():
+        center = details[0]
+        cv2.circle(image, center, 5, (0,0,255), -1)
+
+        corner = ArUco_corners[int(ids)]
+        cv2.circle(image, (int(corner[0][0]), int(corner[0][1])), 5, (50, 50, 50), -1)
+        cv2.circle(image, (int(corner[1][0]), int(corner[1][1])), 5, (0, 255, 0), -1)
+        cv2.circle(image, (int(corner[2][0]), int(corner[2][1])), 5, (128, 0, 255), -1)
+        cv2.circle(image, (int(corner[3][0]), int(corner[3][1])), 5, (255, 255, 255), -1)
+
+        tl_tr_center_x = int((corner[0][0] + corner[1][0]) / 2)
+        tl_tr_center_y = int((corner[0][1] + corner[1][1]) / 2) 
+
+        cv2.line(image,center,(tl_tr_center_x, tl_tr_center_y),(255,0,0),5)
+        display_offset = 2*int(math.sqrt((tl_tr_center_x - center[0])**2+(tl_tr_center_y - center[1])**2))
+        cv2.putText(image,str(ids),(center[0]+int(display_offset/2),center[1]),cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+        angle = details[1]
+        cv2.putText(image,str(angle),(center[0]-display_offset,center[1]),cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    return image
+    
+#####################################################################################
+
+def perspective_transform(image):
+
+    """
+    Purpose:
+    ---
+    This function takes the image as an argument and returns the image after 
+    applying perspective transform on it. Using this function, you should
+    crop out the arena from the full frame you are receiving from the 
+    overhead camera feed.
+
+    HINT:
+    Use the ArUco markers placed on four corner points of the arena in order
+    to crop out the required portion of the image.
+
+    Input Arguments:
+    ---
+    `image` :	[ numpy array ]
+            numpy array of image returned by cv2 library 
+
+    Returns:
+    ---
+    `warped_image` : [ numpy array ]
+            return cropped arena image as a numpy array
+    
+    Example call:
+    ---
+    warped_image = perspective_transform(image)
+    """   
+    warped_image = [] 
+#################################  ADD YOUR CODE HERE  ###############################
+    
+######################################################################################
+
+    return warped_image
+
+def transform_values(image):
+
+    """
+    Purpose:
+    ---
+    This function takes the image as an argument and returns the 
+    position and orientation of the ArUco marker (with id 5), in the 
+    CoppeliaSim scene.
+
+    Input Arguments:
+    ---
+    `image` :	[ numpy array ]
+            numpy array of image returned by camera
+
+    Returns:
+    ---
+    `scene_parameters` : [ list ]
+            a list containing the position and orientation of ArUco 5
+            scene_parameters = [c_x, c_y, c_angle] where
+            c_x is the transformed x co-ordinate [float]
+            c_y is the transformed y co-ordinate [float]
+            c_angle is the transformed angle [angle]
+    
+    HINT:
+        Initially the image should be cropped using perspective transform 
+        and then values of ArUco (5) should be transformed to CoppeliaSim
+        scale.
+    
+    Example call:
+    ---
+    scene_parameters = transform_values(image)
+    """   
+    scene_parameters = []
+#################################  ADD YOUR CODE HERE  ###############################
+ 
+######################################################################################
+
+    return scene_parameters
+
+
+def set_values(scene_parameters):
+    """
+    Purpose:
+    ---
+    This function takes the scene_parameters, i.e. the transformed values for
+    position and orientation of the ArUco marker, and sets the position and 
+    orientation in the CoppeliaSim scene.
+
+    Input Arguments:
+    ---
+    `scene_parameters` :	[ list ]
+            list of co-ordinates and orientation obtained from transform_values()
+            function
+
+    Returns:
+    ---
+    None
+
+    HINT:
+        Refer Regular API References of CoppeliaSim to find out functions that can
+        set the position and orientation of an object.
+    
+    Example call:
+    ---
+    set_values(scene_parameters)
+    """   
+    aruco_handle = sim.getObject('/aruco_5')
+#################################  ADD YOUR CODE HERE  ###############################
+
+######################################################################################
+
+    return None
+
+if __name__ == "__main__":
+    client = RemoteAPIClient()
+    sim = client.getObject('sim')
+    task_1b = __import__('task_1b')
+#################################  ADD YOUR CODE HERE  ################################
+
+#######################################################################################
+
+
+
+    
